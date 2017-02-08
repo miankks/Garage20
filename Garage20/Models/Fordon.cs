27aacfd05265bf1ac01ee7db91acdad9027e83a2
Jocke <jocke769@hotmail.com>
2017-02-08 15:54:23 +0100

    Skapar klassen Fordon

---
+++ Garage20/Models/Fordon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage20.Models
+{
+    public class Fordon
+    {
+        public string Typ { get; set; }
+        public string RegNr { get; set; }
+        public string Färg { get; set; }
+        public string Märke { get; set; }
+        public string Modell { get; set; }
+        public int AntalHjul { get; set; }
+    }
+}
