2afd3dd8e818400842dd7492f9b7d751e43823b3
miankks <bilal.jan085@gmail.com>
2017-02-18 17:52:10 +0100

    qo

--- Garage20/Migrations/Configuration.cs
+++ Garage20/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Garage20.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -7,6 +8,7 @@ namespace Garage20.Migrations
 
     internal sealed class Configuration : DbMigrationsConfiguration<Garage20.DAL.Garage20Context>
     {
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
@@ -14,18 +16,38 @@ namespace Garage20.Migrations
 
         protected override void Seed(Garage20.DAL.Garage20Context context)
         {
-            //  This method will be called after migrating to the latest version.
+            var medlemmar = new[]
+           {
+                new Medlem {  MedlemsId=0,Förnamn="Mikael", Efternamn="Gonzales"},
+                new Medlem { MedlemsId=1,Förnamn="Anna", Efternamn="Anka"},
+                new Medlem { MedlemsId=2,Förnamn="Adam", Efternamn="Andersson"},
+                new Medlem { MedlemsId=3,Förnamn="Bertil", Efternamn="Börjesson"},
+                new Medlem { MedlemsId=4,Förnamn="Christer", Efternamn="Carlsson"},
+            };
+            context.Medlemmar.AddRange(medlemmar);
+            context.SaveChanges();
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var fordonstyper = new[]
+            {
+                new Fordonstyp { FordonstypId=0,Typ="Bil" },
+                new Fordonstyp { FordonstypId=1,Typ="Motorcykel" },
+                new Fordonstyp { FordonstypId=2,Typ="Båt" },
+                new Fordonstyp { FordonstypId=3,Typ="Buss" },
+                new Fordonstyp { FordonstypId=4,Typ="Flygplan" }
+            };
+            context.Fordonstyper.AddRange(fordonstyper);
+            context.SaveChanges();
+
+            var fordon = new[]
+            {
+                new Fordon { Id, MedlemsId=medlemmar[0].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=DateTime.Now,  RegNr = "BIL111", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
+                new Fordon { MedlemsId=medlemmar[1].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=DateTime.Now,  RegNr = "BIL222", Märke = "Volvo", Modell = "850", Färg = "Gul", AntalHjul = 4},
+                new Fordon { MedlemsId=medlemmar[2].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=DateTime.Now,  RegNr = "BIL333", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
+                new Fordon { MedlemsId=medlemmar[3].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=DateTime.Now,  RegNr = "BIL444", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
+                new Fordon { MedlemsId=medlemmar[4].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=DateTime.Now,  RegNr = "BIL555", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
+            };
+            context.Fordons.AddRange(fordon);
+            context.SaveChanges();
         }
     }
 }
