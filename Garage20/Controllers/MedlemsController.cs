b8fbc70cb51b536ced847df1fc2ce4f63f4a7e47
miankks <bilal.jan085@gmail.com>
2017-02-18 17:43:16 +0100

    grage25

---
+++ Garage20/Controllers/MedlemsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Garage20.DAL;
+using Garage20.Models;
+
+namespace Garage20.Controllers
+{
+    public class MedlemsController : Controller
+    {
+        private Garage20Context db = new Garage20Context();
+
+        // GET: Medlems
+        public ActionResult Index()
+        {
+            return View(db.Medlemmar.ToList());
+        }
+
+        // GET: Medlems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medlem medlem = db.Medlemmar.Find(id);
+            if (medlem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medlem);
+        }
+
+        // GET: Medlems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medlems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MedlemsId,Förnamn,Efternamn")] Medlem medlem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medlemmar.Add(medlem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medlem);
+        }
+
+        // GET: Medlems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medlem medlem = db.Medlemmar.Find(id);
+            if (medlem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medlem);
+        }
+
+        // POST: Medlems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MedlemsId,Förnamn,Efternamn")] Medlem medlem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medlem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(medlem);
+        }
+
+        // GET: Medlems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medlem medlem = db.Medlemmar.Find(id);
+            if (medlem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medlem);
+        }
+
+        // POST: Medlems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medlem medlem = db.Medlemmar.Find(id);
+            db.Medlemmar.Remove(medlem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Garage20/Migrations/Configuration.cs
+++ Garage20/Migrations/Configuration.cs
@@ -1,6 +1,5 @@
 namespace Garage20.Migrations
 {
-    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -11,61 +10,22 @@ namespace Garage20.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            ContextKey = "Garage20.DAL.Garage20Context";
         }
 
         protected override void Seed(Garage20.DAL.Garage20Context context)
         {
-            var datetime = DateTime.Now;
-            /*context.Fordons.AddOrUpdate(p=>p.Id,
-                new Fordon {Tid=datetime, RegNr = "BIL111", Fordonstyper = Typ.Bil, Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4 },
-                new Fordon {Tid=datetime, RegNr = "BIL222", Typ = Typ.Bil, Märke = "Volkswagen", Modell = "Golf", Färg = "Grå", AntalHjul = 4 },
-                new Fordon {Tid=datetime, RegNr = "BIL333", Typ = Typ.Bil, Märke = "Mercedes-Benz", Modell = "220", Färg = "Svart", AntalHjul = 4 },
-                new Fordon {Tid=datetime, RegNr = "BIL444", Typ = Typ.Bil, Märke = "Ferrari", Modell = "Testarossa", Färg = "Röd", AntalHjul = 4 },
-                new Fordon {Tid=datetime, RegNr = "BIL555", Typ = Typ.Bil, Märke = "Citroën", Modell = "C4", Färg = "Vit", AntalHjul = 4 },
-                new Fordon {Tid=datetime, RegNr = "BUS111", Typ = Typ.Buss, Märke = "Scania", Modell = "850", Färg = "Gul", AntalHjul = 4 },
-                new Fordon {Tid=datetime, RegNr = "BUS222", Typ = Typ.Buss, Märke = "Renault", Modell = "Golf", Färg = "Röd", AntalHjul = 6 },
-                new Fordon {Tid=datetime, RegNr = "BUS333", Typ = Typ.Buss, Märke = "Mercedes-Benz", Modell = "220", Färg = "Blå", AntalHjul = 6 },
-                new Fordon {Tid=datetime, RegNr = "BUS444", Typ = Typ.Buss, Märke = "Volvo", Modell = "Testarossa", Färg = "Svart", AntalHjul = 8 },
-                new Fordon {Tid=datetime, RegNr = "BUS555", Typ = Typ.Buss, Märke = "MAN", Modell = "C4", Färg = "Grön", AntalHjul = 10 },
-                new Fordon {Tid=datetime, RegNr = "MCF111", Typ = Typ.Motorcykel, Märke = "Yamaha", Modell = "500cc", Färg = "Gul", AntalHjul = 2 },
-                new Fordon {Tid=datetime, RegNr = "MCK222", Typ = Typ.Motorcykel, Märke = "Honda", Modell = "FCX", Färg = "Röd", AntalHjul = 2 },
-                new Fordon {Tid=datetime, RegNr = "MCG333", Typ = Typ.Motorcykel, Märke = "Harley-Davidson", Modell = "Softail", Färg = "Blå", AntalHjul = 2 },
-                new Fordon {Tid=datetime, RegNr = "MCH444", Typ = Typ.Motorcykel, Märke = "Kawasaki", Modell = "ZX-12R", Färg = "Svart", AntalHjul = 2 },
-                new Fordon {Tid=datetime, RegNr = "MCJ555", Typ = Typ.Motorcykel, Märke = "Aprilia", Modell = "RS125", Färg = "Grön", AntalHjul = 3 });*/
+            //  This method will be called after migrating to the latest version.
 
-            var medlemmar = new[] 
-            {
-                new Medlem { Förnamn="Mikael", Efternamn="Gonzales"},
-                new Medlem { Förnamn="Anna", Efternamn="Anka"},
-                new Medlem { Förnamn="Adam", Efternamn="Andersson"},
-                new Medlem { Förnamn="Bertil", Efternamn="Börjesson"},
-                new Medlem { Förnamn="Christer", Efternamn="Carlsson"},
-            };
-            context.Medlemmar.AddRange(medlemmar);
-            context.SaveChanges();
-
-            var fordonstyper = new[]
-            {
-                new Fordonstyp { Typ="Bil" },
-                new Fordonstyp { Typ="Motorcykel" },
-                new Fordonstyp { Typ="Båt" },
-                new Fordonstyp { Typ="Buss" },
-                new Fordonstyp { Typ="Flygplan" }
-            };
-            context.Fordonstyper.AddRange(fordonstyper);
-            context.SaveChanges();
-
-            var fordon = new[]
-            {
-                new Fordon { MedlemsId=medlemmar[0].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=datetime,  RegNr = "BIL111", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
-                new Fordon { MedlemsId=medlemmar[1].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=datetime,  RegNr = "BIL222", Märke = "Volvo", Modell = "850", Färg = "Gul", AntalHjul = 4},
-                new Fordon { MedlemsId=medlemmar[2].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=datetime,  RegNr = "BIL333", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
-                new Fordon { MedlemsId=medlemmar[3].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=datetime,  RegNr = "BIL444", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
-                new Fordon { MedlemsId=medlemmar[4].MedlemsId,  FordonstypId=fordonstyper[0].FordonstypId , Tid=datetime,  RegNr = "BIL555", Märke = "Volvo", Modell = "850", Färg = "Röd", AntalHjul = 4},
-            };
-            context.Fordons.AddRange(fordon);
-            context.SaveChanges();
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
         }
     }
 }
